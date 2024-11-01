adf4e0821b6fbe631e2ecd901db6e751e510214e
Mikhail <polyakov.mihail@skbkontur.ru>
2024-11-01 21:40:33 +0300

    done lab4

---
+++ MyPortfolio/js/script.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    function setActiveMenuItem() {
+        const menuItems = document.querySelectorAll('.nav__item a');
+        const currentPath = window.location.pathname;
+
+        menuItems.forEach(item => {
+            if (item.pathname === currentPath) {
+                item.classList.add('active');
+            }
+        });
+    }
+
+    function showLoadTime() {
+        const loadTime = window.performance.now();
+        const footer = document.querySelector('footer');
+
+        const loadTimeElement = document.createElement('p');
+        loadTimeElement.textContent = `Время загрузки страницы: ${loadTime.toFixed(2)} мс`;
+        footer.appendChild(loadTimeElement);
+    }
+
+    window.addEventListener('load', () => {
+        setActiveMenuItem();
+        showLoadTime();
+    });
+})();
